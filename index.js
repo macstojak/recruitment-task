e41dd81da2b82760ce974f55a421db23c4811b8d
macstojak <maciej.stojak89@wp.pl>
2020-08-20 21:29:56 +0200

    Fetch movies done, next adding movie

--- index.js
+++ index.js
@@ -4,25 +4,42 @@ const express = require("express");
 const app = express();
 const port = 4000;
 const fs = require("fs");
-const db = require("./data/db.json");
+const path = require("path");
+const bodyParser = require("body-parser");
 
-const getData = () =>{
-   fs.readFile("./data/db.json", (error, result)=>{
-        if(error){
-            console.log("Error fetching data:", error)
-         }else{
-            return JSON.parse(result);
-         } 
-    })   
+app.use(express.static(path.join(__dirname, "data")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+const getMovies = () =>{
+    return new Promise((resolve, reject) =>{
+        fs.readFile(`${__dirname}/data/db.json`, "utf-8", (error, result)=>{
+            error ? reject("Error fetching data:", error) : resolve((JSON.parse(result)).movies);             
+        })   
+    })
 }
 
-app.get("/",  async ()=>{
+app.get("/",  async (req, res)=>{
     try{
-        const data = await getData();
-        console.log("Here's your data:",data);
+        const data = await getMovies();
+        res.send(data);     
     }catch(e){
-        console.log("Error ocurred:",e);
+        res.status(404).send("Error ocurred:"+e);
     }
+})
+
+app.post("/add", async (req, res)=>{
+    const data = await getMovies();
+    const id = data[data.length-1].id+1;
+    let movie =req.body;
+    movie.id = id;
+    data.push(movie);
+    try{
+        fs.writeFile(`${__dirname}/data/db.json`, JSON.stringify(data), "UTF-8", ()=>{})
+       
+    }catch(error){
+        console.log(error);
+    }
+  
 })
 
 app.listen(port, ()=>{
